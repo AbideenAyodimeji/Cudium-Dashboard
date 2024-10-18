57e84dc2b23174d25e834b6137f9bcc4ab287509
AbideenAyodimeji <abideenoygbenro2018@gmail.com>
2024-10-18 08:01:39 +0100

    Updated NotFoundPage/Page.js and NotFoundPage.module.css

--- app/Authentication/NotFoundPage/page.js
+++ app/Authentication/NotFoundPage/page.js
@@ -1,61 +1,61 @@
-// import '@styles/NotFoundPage.module.css'
-// import Image from 'next/image'
+import styles from '@styles/NotFoundPage.module.css'
+import Image from 'next/image'
 
-// export default function NotFoundPage() {
-//   return (
-//     <section className={styles.section}>
-//       <Image
-//         src='/Images/leftTopBgImg.svg'
-//         alt='Background Image'
-//         width={100}
-//         height={100}
-//         className={styles.leftTopBgImg}
-//       />
-//       <Image
-//         src='/Images/rightBgImg.svg'
-//         alt='Background Image'
-//         width={100}
-//         height={100}
-//         className={styles.rightBgImg}
-//       />
-//       <Image
-//         src='/Images/leftBtmBgImg.svg'
-//         alt='Background Image'
-//         width={100}
-//         height={100}
-//         className={styles.leftBtmBgImg}
-//       />
+export default function NotFoundPage() {
+  return (
+    <section className={styles.section}>
+      <Image
+        src='/Images/leftTopBgImg.svg'
+        alt='Background Image'
+        width={100}
+        height={100}
+        className={styles.leftTopBgImg}
+      />
+      <Image
+        src='/Images/rightBgImg.svg'
+        alt='Background Image'
+        width={100}
+        height={100}
+        className={styles.rightBgImg}
+      />
+      <Image
+        src='/Images/leftBtmBgImg.svg'
+        alt='Background Image'
+        width={100}
+        height={100}
+        className={styles.leftBtmBgImg}
+      />
 
-//       <header className={`${styles.header} ${styles.justifyCenter}`}>
-//         <div className={` ${styles.flexRow} ${styles.alignCenter} `}>
-//           <Image
-//             src='/Images/cudiumLogo.png'
-//             alt='Cudium Logo'
-//             width={50}
-//             height={50}
-//             className={styles.cudiumLogo}
-//           />
-//           <p className={styles.headerText}>Cudium</p>
-//         </div>
-//       </header>
+      <header className={`${styles.header} ${styles.justifyCenter}`}>
+        <div className={` ${styles.flexRow} ${styles.alignCenter} `}>
+          <Image
+            src='/Images/cudiumLogo.png'
+            alt='Cudium Logo'
+            width={50}
+            height={50}
+            className={styles.cudiumLogo}
+          />
+          <p className={styles.headerText}>Cudium</p>
+        </div>
+      </header>
 
-//       <main className={styles.main}>
-//         <Image
-//           src='/Images/404-Img.svg'
-//           alt='Cudium Logo'
-//           width={50}
-//           height={50}
-//           className={styles.cudiumLogo}
-//         />
-//         <div className={`${styles.flexColumn} ${styles.alignCenter}`}>
-//           <h3 className={styles.mainHeading}>Page not found</h3>
-//           <p className={styles.mainSubHeading}>
-//             This page you are looking for doesn’t exist or an other error
-//             occurred. Go back, or head over to cudium.com to choose a new
-//             direction
-//           </p>
-//         </div>
-//       </main>
-//     </section>
-//   )
-// }
+      <main className={styles.main}>
+        <Image
+          src='/Images/404-Img.svg'
+          alt='Cudium Logo'
+          width={50}
+          height={50}
+          className={styles.cudiumLogo}
+        />
+        <div className={`${styles.flexColumn} ${styles.alignCenter}`}>
+          <h3 className={styles.mainHeading}>Page not found</h3>
+          <p className={styles.mainSubHeading}>
+            This page you are looking for doesn’t exist or an other error
+            occurred. Go back, or head over to cudium.com to choose a new
+            direction
+          </p>
+        </div>
+      </main>
+    </section>
+  )
+}
